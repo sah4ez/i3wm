067c3e8a898352334213b6b1cb61df17d0f49951
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-06-29 22:10:21 +0300

    fix loading monitoring and docker workspace

--- monitoring.sh
+++ monitoring.sh
@@ -3,4 +3,4 @@
 /bin/sh -c "gnome-terminal -e htop"
 /bin/sh -c "gnome-terminal -e 'sudo iotop'"
 /bin/sh -c "gnome-terminal -e 'sudo iftop'"
-/bin/sh -c "gnome-terminal -e 'conky'"
+#/bin/sh -c "gnome-terminal -e 'conky'"
