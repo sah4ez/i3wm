709658105f5079c10a8917447baf2716542c5ef2
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-07-03 09:17:00 +0300

    fix loading Docker workspace

--- docker-tools.sh
+++ docker-tools.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+PROCESS=$(ps aux | egrep "(htop|iftop|iotop)" | wc -l)
+while [ $PROCESS -ne 6 ] 
+do
+  sleep 2
+  PROCESS=$(ps aux | egrep "(htop|iftop|iotop)" | wc -l)
+done
+
+i3-msg workspace '10: Docker'
 /bin/sh -c "gnome-terminal"
 /bin/sh -c "gnome-terminal"
 /bin/sh -c "gnome-terminal -e 'docker stats'" && /bin/sh -c "i3-msg 'move down'" && /bin/sh -c "i3-msg resize shrink height 5ppt" && /bin/sh -c "i3-msg resize shrink height 5ppt"
