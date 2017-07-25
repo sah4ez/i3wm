207f92d9c9a384d7d76ffc396406d7663ad35442
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-07-25 13:46:00 +0300

    fixed replace for VPN

--- check_vpn.sh
+++ check_vpn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nmcli connection show | grep vpn | awk '{print $4}' | sed -r 's/--/off/g' | sed -r 's/enp4s0/on/g'
+nmcli connection show | grep vpn | awk '{print $4}' | sed -r 's/--/off/g' | sed -r 's/^enp0s\w+/on/g'
