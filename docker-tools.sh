757608878de00fec06b21bba19b1663dfeeef2be
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-08-21 13:51:18 +0300

    change statistics docker to ctop

--- docker-tools.sh
+++ docker-tools.sh
@@ -10,5 +10,4 @@ done
 i3-msg workspace '10: Docker'
 /bin/bash -c "gnome-terminal"
 /bin/bash -c "gnome-terminal"
-/bin/bash -c "gnome-terminal -e 'docker stats'" && /bin/bash -c "i3-msg 'move down'" && /bin/bash -c "i3-msg resize shrink height 5ppt" && /bin/bash -c "i3-msg resize shrink height 5ppt"
-/bin/bash -c "i3-msg 'split h'" && /bin/bash -c "gnome-terminal -e '$HOME/.i3/docker_ps.sh'" 
+/bin/bash -c "gnome-terminal -e 'ctop'" && /bin/bash -c "i3-msg 'move down'" && /bin/bash -c "i3-msg resize shrink height 5ppt" && /bin/bash -c "i3-msg resize shrink height 5ppt"
