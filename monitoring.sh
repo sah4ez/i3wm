81fef6d92bcf591b4deee0b331db135c47d797f2
Alexandr Kozlenkov <sah4ez32@gmail.com>
2018-03-21 09:29:26 +0300

    added interface wifi for iftop

--- monitoring.sh
+++ monitoring.sh
@@ -2,5 +2,5 @@
 
 /bin/sh -c "gnome-terminal -e htop"
 /bin/sh -c "gnome-terminal -e 'sudo iotop'"
-/bin/sh -c "gnome-terminal -e 'sudo iftop'"
+/bin/sh -c "gnome-terminal -e 'sudo iftop -i wlx503eaa362dee'"
 #/bin/sh -c "gnome-terminal -e 'conky'"
