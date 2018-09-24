61647448acf687fad205d7e42ed9d40c0ea7694a
Alexandr Kozlenkov <sah4ez32@gmail.com>
2018-09-24 08:06:11 +0300

    Fixed check brightness file

--- brightness.sh
+++ brightness.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f ${HOME}/.i3/brightness ] ;
+if [ ! -f ${HOME}/.i3/brightness ] ;
 then
 	echo "Set default valeus for brightness"
-	echo '.5' > ${HOME}/.i3/brightness
+	echo ".5" > ${HOME}/.i3/brightness
 fi
 
 if [ -f /tmp/brightness.lock ] ;
