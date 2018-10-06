adfa5f848662525642ca4fe9bb506b23653c59a1
Alexandr Kozlenkov <sah4ez32@gmail.com>
2018-10-06 22:51:23 +0300

    Remove nmcli from config and wifi level signal checker

--- check_wifi.sh
+++ check_wifi.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-ok=$(nmcli c | grep OpenWrt | awk '{print $4}')
-if [ "$ok" == "--" ]; 
-then 
-    echo "NO"
-else
-    nmcli device wifi list | grep OpenWrt | awk '{print $8}'
+level=$(/sbin/iwconfig wlx503eaa362dee | egrep -i --color '(quality|Signal)' | awk -F '=' '{print $2}' | awk -F '/' '{print $1}')
+level_str="▁___"
+if [ $level -gt 75 ]
+then
+	level_str="▁▃▅▇"
+elif [ $level -gt 50 ]
+then
+	level_str="▁▃▅_"
+elif [ $level -gt 25 ]
+then
+	level_str="▁▃__"
 fi
+
+echo "$level_str"
--- check_wifi_signal.sh
+++ check_wifi_signal.sh
@@ -2,16 +2,16 @@
 LANG=en_EN.UTF-8
 level=$(~/.i3/check_wifi.sh)
 case $level in 
-    "****")
+    "▁▃▅▇")
         echo "#00ff00"
         ;;
-    "***")
+    "▁▃▅_")
         echo "#007d00"
         ;;
-    "**")
+    "▁▃__")
         echo "#e06a00"
         ;;
-    "*")
+    "▁___")
         echo "#902200"
         ;;
     "NO")
