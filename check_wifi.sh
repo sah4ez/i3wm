d3c15662c5fbb1122b190340384542530467513e
Alexandr Kozlenkov <sah4ez32@gmail.com>
2019-05-01 18:17:26 +0300

    Fixed wifi signal status bar

--- check_wifi.sh
+++ check_wifi.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-ok=$(nmcli c | grep wlp2s0 | awk '{print $4}')
-if [ "$ok" == "--" ]; 
-then 
-    echo "NO"
-else
-    nmcli device wifi list | egrep "\*" | awk '{print $8}'
+level=$(/sbin/iwconfig wlp2s0 | egrep -i --color '(quality|Signal)' | awk -F '=' '{print $2}' | awk -F '/' '{print $1}')
+level_str="▁___"
+if [ $level -gt 75 ]
+then
+    level_str="▁▃▅▇"
+elif [ $level -gt 50 ]
+then
+    level_str="▁▃▅_"
+elif [ $level -gt 25 ]
+then
+    level_str="▁▃__"
 fi
+
+echo "$level_str"
--- check_wifi_signal.sh
+++ check_wifi_signal.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 LANG=en_EN.UTF-8
 level=$(~/.i3/check_wifi.sh)
-case $level in 
-    "****")
+case $level in
+    "▁▃▅▇")
         echo "#00ff00"
         ;;
-    "***")
+    "▁▃▅_")
         echo "#007d00"
         ;;
-    "**")
+    "▁▃__")
         echo "#e06a00"
         ;;
-    "*")
+    "▁___")
         echo "#902200"
         ;;
     "NO")
@@ -21,4 +21,3 @@ case $level in
         echo "#321321"
         ;;
 esac
-
